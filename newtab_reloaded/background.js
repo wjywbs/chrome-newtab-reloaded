85560b490d075b6ec083fc054f278216476393b3
wjywbs <wjywbs@gmail.com>
2014-05-01 14:05:48 -0400

    Clean up RGBaster.

--- newtab_reloaded/background.js
+++ newtab_reloaded/background.js
@@ -184,7 +184,6 @@ function getSettings() {
   };
 
   var getImageData = function(img, loaded){
-
     var imgObj = new Image();
     var imgSrc = img.src || img;
 
@@ -202,7 +201,6 @@ function getSettings() {
     };
     
     imgObj.src = imgSrc;
-
   };
 
   var makeRGB = function(name){
@@ -220,57 +218,56 @@ function getSettings() {
 
   RGBaster.colors = function(img, success, paletteSize){
     getImageData(img, function(data){
+      var length        = data.length,
+          colorCounts   = {},
+          rgbString     = '',
+          rgb           = [],
+          colors        = {
+            dominant: { name: '', count: 0 },
+            palette:  Array.apply(null, Array(paletteSize || PALETTESIZE)).map(Boolean).map(function(a){ return { name: '0,0,0', count: 0 } }) 
+          };
 
-              var length        = data.length,
-                  colorCounts   = {},
-                  rgbString     = '',
-                  rgb           = [],
-                  colors        = { 
-                    dominant: { name: '', count: 0 },
-                    palette:  Array.apply(null, Array(paletteSize || PALETTESIZE)).map(Boolean).map(function(a){ return { name: '0,0,0', count: 0 } }) 
-                  };
+      // Loop over all pixels, in BLOCKSIZE iterations.
+      var i = 0;
+      while ( i < length ) {
+        rgb[0] = data[i];
+        rgb[1] = data[i+1];
+        rgb[2] = data[i+2];
+        rgbString = rgb.join(",");
 
-              // Loop over all pixels, in BLOCKSIZE iterations.
-              var i = 0;
-              while ( i < length ) {
-                rgb[0] = data[i];
-                rgb[1] = data[i+1];
-                rgb[2] = data[i+2];
-                rgbString = rgb.join(",");
+        // Keep track of counts.
+        if ( rgbString in colorCounts ) {
+          colorCounts[rgbString] = colorCounts[rgbString] + 1;
+        }
+        else{
+          colorCounts[rgbString] = 1;
+        }
 
-                // Keep track of counts.
-                if ( rgbString in colorCounts ) {
-                  colorCounts[rgbString] = colorCounts[rgbString] + 1; 
-                } 
-                else{
-                  colorCounts[rgbString] = 1;
-                }
-
-                // Find dominant and palette, ignoring black/white pixels.
-                if ( rgbString !== "0,0,0" && rgbString !== "255,255,255" ) {
-                  var colorCount = colorCounts[rgbString]
-                  if ( colorCount > colors.dominant.count ){
-                    colors.dominant.name = rgbString;
-                    colors.dominant.count = colorCount;
-                  } else {
-                    colors.palette.some(function(c){
-                      if ( colorCount > c.count ) {
-                        c.name = rgbString;
-                        c.count = colorCount;
-                        return true;
-                      }
-                    });
-                  }
-                }
-
-                // Increment!
-                i += BLOCKSIZE * 4;
+        // Find dominant and palette, ignoring black/white pixels.
+        if ( rgbString !== "0,0,0" && rgbString !== "255,255,255" ) {
+          var colorCount = colorCounts[rgbString]
+          if ( colorCount > colors.dominant.count ){
+            colors.dominant.name = rgbString;
+            colors.dominant.count = colorCount;
+          } else {
+            colors.palette.some(function(c){
+              if ( colorCount > c.count ) {
+                c.name = rgbString;
+                c.count = colorCount;
+                return true;
               }
+            });
+          }
+        }
 
-              success && success({
-                dominant: makeRGB(colors.dominant.name),
-                palette:  mapPalette(colors.palette)
-              });
+        // Increment!
+        i += BLOCKSIZE * 4;
+      }
+
+      success && success({
+        dominant: makeRGB(colors.dominant.name),
+        palette:  mapPalette(colors.palette)
+      });
     });
   }
 // end RGBster merge
